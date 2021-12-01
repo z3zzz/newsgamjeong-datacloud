d594f8d37ba2572b9cd932faf4a4a99b2202939d
kwangtae <zabra7946@naver.com>
2021-12-01 14:14:56 +0000

    made json files directory

--- concatParts2.py
+++ concatParts2.py
@@ -3,57 +3,94 @@ import json
 from pprint import pprint
 
 ## 필수!! db, col 이름 변수 설정
-db_from_name = 'final_data'
-col_from_name = 'newses'
-
-db_to_name = "final_data"
-col_to_name = 'statistics'
+db_name = 'result'
+col_name = 'practice10'
 
 connection = pymongo.MongoClient("mongodb://localhost:27017/")
-db = connection.get_database(db_from_name)
-col = db.get_collection(col_from_name)
+db = connection.get_database(db_name)
+col = db.get_collection(col_name)
 
-col_selectors = db.get_collection("selectors")
-selectors = col_selectors.find_one({})
+def supplement_ranking(news_ranking):
+    positive_cnt = len(news_ranking["positive_ranking"])
+    negative_cnt = len(news_ranking["negative_ranking"])
+    normal_cnt = len(news_ranking["normal_ranking"])
+    newses_cnt = len(news_ranking["number_of_newses"])
 
-'''
-dates, keywords, text_companies
-'''
+    if positive_cnt < 5:
+        news_ranking["positive_ranking"] += [['',0]] * (5 - positive_cnt)
+        news_ranking["negative_ranking"] += [['',0]] * (5 - negative_cnt)
+        news_ranking["normal_ranking"] += [['',0]] * (5 - normal_cnt)
+        for i in range(5 - newses_cnt):
+            news_ranking["number_of_newses"][f'언론사 없음{i+1}'] = 0
 
-db_result = connection.get_database(db_to_name)
-col_result = db_result.get_collection(col_to_name)
+    return news_ranking
 
-sentiment_mapping = {'중립':'normal', '긍정':'positive', '부정':'negative'}
+def convert_count_to_ratio(result2):
+    minimum_number, cnt = get_average_number_of_newses(result2)
+    result3 = {"positive_ranking": [], "negative_ranking": [], "normal_ranking": [], "number_of_newses": {}, "criteria": minimum_number, "number_of_companies": cnt}
+
+    for company in result2["positive_ranking"].keys():
+        positive_count = result2["positive_ranking"][company]
+        negative_count = result2["negative_ranking"][company]
+        normal_count = result2["normal_ranking"][company]
+        total = positive_count + negative_count + normal_count
+        if total == 0:
+            continue
+
+        positive_ratio = (positive_count / total) * 100
+        negative_ratio = (negative_count / total) * 100
+        normal_ratio = (normal_count / total) * 100
+
+        if total > minimum_number:
+            result3["positive_ranking"].append([company,round(positive_ratio,1)])
+            result3["negative_ranking"].append([company,round(negative_ratio,1)])
+            result3["normal_ranking"].append([company,round(normal_ratio,1)])
+            result3["number_of_newses"][company] = total
+
+
+    result3["positive_ranking"] = sorted(result3["positive_ranking"], key=lambda x:x[1], reverse=True )[:5]
+    result3["negative_ranking"] = sorted(result3["negative_ranking"], key=lambda x:x[1], reverse=True )[:5]
+    result3["normal_ranking"] = sorted(result3["normal_ranking"], key=lambda x:x[1], reverse=True )[:5]
+
+    return result3
+
+def get_average_number_of_newses(result2):
+    total = 0
+    cnt = 0
+    for company in result2["positive_ranking"].keys():
+        positive_count = result2["positive_ranking"][company]
+        negative_count = result2["negative_ranking"][company]
+        normal_count = result2["normal_ranking"][company]
+
+        total += positive_count + negative_count + normal_count
+        cnt += 1
+
+    print(total,cnt)
+    return round(total / cnt), cnt
+
 
 # 1
-def date_total_keyword_specific_company_specific():
-    for keyword in selectors["keywords"]:
-        result = []
-        for company in selectors["text_companies"]:
-            temp = {
-                "date": "total",
-                "keyword": keyword,
-                "company": company,
-                "total": 0,
-                "positive": 0,
-                "negative": 0,
-                "normal": 0
-            }
+def date_total_keyword_specific_company_specific(keyword, company):
+    result = {
+        "total": 0,
+        "positive": 0,
+        "negative": 0,
+        "normal": 0,
+        "infections": 0
+    }
 
-            for data in col.find({"category":keyword,"text_company":company}):
-                temp["total"] += 1
-                temp[sentiment_mapping[data['label']]] += 1
+    data = col.find_one({"날짜":"전체","키워드":keyword,"언론사":company})
 
-            result.append(temp)
+    result["total"] += data["전체"]
+    result["positive"] += data["긍정"]
+    result["negative"] += data["부정"]
+    result["normal"] += data["노말"]
 
-        col_result.insert_many(result)
-        print(keyword)
-
-    return "done"
+    return result
 
 '''
-업데이트 경우 외 실행 금지
-pprint(date_total_keyword_specific_company_specific())
+코드 테스트용
+pprint(date_total_keyword_specific_company_specific("코로나", "조선일보"))
 '''
 
 # 2
@@ -191,8 +228,8 @@ def date_specific_keyword_total_company_total(date):
 
 '''
 코드 테스트용
-pprint(date_specific_keyword_total_company_total("2021.01.02"))
 '''
+pprint(date_specific_keyword_total_company_total("2021.01.02"))
 
 # 5
 def date_total_keyword_specific_company_total(keyword):
@@ -249,8 +286,8 @@ def date_total_keyword_specific_company_total(keyword):
 
 '''
 코드 테스트용
-pprint(date_total_keyword_specific_company_total("코로나"))
 '''
+pprint(date_total_keyword_specific_company_total("코로나"))
 
 # 6
 def date_total_keyword_total_company_specific(company):
@@ -352,14 +389,7 @@ def date_total_keyword_total_company_total():
 
     return result
 
-
-
-
 '''
-메인함수
-make_news_list()
-테스트용
-print(getSentiment([10,6,3]))
-pprint(getLineGraphData(1))
-pprint(prepare_data_1())
+코드 테스트용
+pprint(date_total_keyword_total_company_total())
 '''
